56471aa7828893df9ee199e74ced3104e859e8ab
pham quang minh <minhpq1203@gmail.com>
2019-06-24 17:33:58 +0700

    Tai len

---
+++ MvcMusicStore/MvcMusicStore/EntityContext/MusicStoreEntities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.EntityContext
+{
+    public class MusicStoreEntities : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+
+        public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<Artist> Artists { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        public MusicStoreEntities()
+            : base("DefaultConnection")
+        {
+
+        }
+    }
+}
